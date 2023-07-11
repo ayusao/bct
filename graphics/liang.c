3cbf1b0bf79c0f4979ac0c3f8ef81b79505e474a
ayusao <077bct006.aayusha@pcampus.edu.np>
2023-07-11 21:23:50 +0545

    Liang-Barsky Line Clipping

--- graphics/liang.c
+++ graphics/liang.c
@@ -1,19 +1,55 @@
-//NOT WOR$KING
 #include <stdio.h>
 #include <graphics.h>
 
-#define ymax 150    // top
-#define ymin 250    // bottom
-#define xmax 350    // right
-#define xmin 250    // left
+#define ymax 400    // top
+#define ymin 100    // bottom
+#define xmax 500    // right
+#define xmin 300   // left
 
-void liang(int x1, int y1, int x2, int y2)
+void clipwindow()
 {
-    int p[4], q[4];
-    float x1_clip, y1_clip, x2_clip, y2_clip;
-    
-    int dx = x2 - x1;
-    int dy = y2 - y1;
+    line(xmin, ymin, xmax, ymin);
+    line(xmax, ymin, xmax, ymax);
+    line(xmax, ymax, xmin, ymax);
+    line(xmin, ymax, xmin, ymin);
+}
+int cliptest(float p, float q, float *u1, float *u2)
+{
+    float r;
+    int retval = TRUE;
+    r = q / p;
+    if (p < 0.0)
+    {       
+        if (r > *u2 )                         
+            retval = FALSE;
+        else if (r > *u1) //max value is to be choosen
+            *u1 = r;
+    }
+    else if (p > 0.0)
+    {
+        if ( r<*u1 )                         
+            retval = FALSE; 
+        else if (r < *u2)
+            *u2 = r;
+    }
+    else //p=0
+    {
+        if (q  < 0.0)
+        {
+            printf("Line is parallel and outside the clipping window \n");
+            retval = FALSE; //Line is parallel and outside the clipping window
+        }    
+    }
+    return (retval);
+}
+void liang(float x1, float y1, float x2, float y2)
+{
+    int flag =0;
+    float p[4], q[4];
+    float u1 = 0;
+    float u2 = 1;
+    float dx = x2 - x1;
+    float dy = y2 - y1;
 
     p[0] = -dx;
     p[1] = dx;
@@ -24,71 +60,43 @@ void liang(int x1, int y1, int x2, int y2)
     q[1] = xmax - x1;
     q[2] = y1 - ymin;
     q[3] = ymax - y1;
-
-    float u1 = 0, u2 = 1;
-
-    for (int i = 0; i < 4; i++)
+    // for (int i = 0; i<4; i++){
+    //     printf("p ==%f\nq == %f\n", p[i], q[i]);
+    // } 
+    if (cliptest(p[0], q[0], &u1, &u2))
+        if (cliptest(p[1], q[1], &u1, &u2))
+            if (cliptest(p[2], q[2], &u1, &u2))
+                if (cliptest(p[3], q[3], &u1, &u2))
+                {
+                    if (u2<1.0)
+                    {
+                        x2 = x1 + u2 * dx;
+                        printf("u2 == %f\n", u2);
+                        y2 = y1 + u2 * dy; 
+                    }
+                    if (u1>0.0)
+                    {
+                        printf("u1 == %f\n", u1);
+                        x1 += u1 * dx;
+                        y1 += u1 * dy;
+                    }
+                    flag =1;
+                    line(x1, y1, x2, y2); 
+                }
+    if (flag == 0)
     {
-        if (p[i] < 0)
-        {
-            float r = (float)q[i] / p[i];
-            printf("r value %d ::: %f\n ",i, r);
-            if (r > u1) //max value is to be choosen
-                u1 = r;
-                printf("u1  pi < 0\t%f\n", u1);
-        }
-        else if (p[i] > 0)
-        {
-            float r = (float)q[i] / p[i];
-            printf("r value %d ::: %f\n ",i, r);
-            if (r < u2)
-                u2 = r;
-                printf("u2  pi > 0\t%f\n", u2);
-        }
-        else
-        {
-            if (q[i] < 0)
-            {
-                printf("Line is parallel and outside the clipping window \n");
-                return; // Rejected; Line is parallel and outside the clipping window
-            }
-            printf("pi == 0\n");
-
-              
-        }
-        
+        printf("Line lies completely outside!");
     }
-    // if (u1 > u2)
-    // {
-    //     printf("Line is completely outside the clipping window\n");
-    //     return; // Line is completely outside the clipping window
-    // }
-    x1_clip = x1 + u1 * dx;
-    y1_clip = y1 + u1 * dy;
-    x2_clip = x1 + u2 * dx;
-    y2_clip = y1 + u2 * dy; 
-
-    line(x1_clip, y1_clip, x2_clip, y2_clip);
-    printf("%f\t%f\t%f\t%f\n", x1_clip, y1_clip, x2_clip, y2_clip);
 }
 int main()
 {
-    int u, v, x, y;
+    float u, v, x, y;
     int gd = DETECT, gm;
     initgraph(&gd, &gm, (char*)"");
-
-    u = 175;    v = 175;    x = 325;    y = 225;
-
-    // Clipping window
-    line(xmin, ymin, xmax, ymin);
-    line(xmax, ymin, xmax, ymax);
-    line(xmax, ymax, xmin, ymax);
-    line(xmin, ymax, xmin, ymin);
-
-    //line(u, v, x, y); // Draw the original line segment
-
+    u = 500;    v = 420;    x = 80;    y = 7;
+    clipwindow();
+    //line(u+20, v, x+20, y); // Draw the original line segment
     liang(u, v, x, y); // Clip the line segment
-
     getch();
     closegraph();
     return 0;
